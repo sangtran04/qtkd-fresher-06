423023c1be682c43ecbc9ac5bc0888b390693219
dovietdong <dovietdong2507@gmail.com>
2022-07-21 09:47:32 +0700

    feat: Vẽ UI trang chính

---
+++ dvdong/amis_crm/src/components/base/BaseTag.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="tag-input">
+    <div 
+    v-for="(tag, index) in tags" 
+    :key="tag" 
+    class="tag-input_tag">
+      <span @click="removeTag(index)">x</span>
+      {{ tag }}
+    </div>
+    <input
+      type="text"
+      placeholder="EnteraTag"
+      class="tag-input_text"
+      @keydown="addTag"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tags: ["hello", "world"],
+    };
+  },
+  methods: {
+    addTag(event) {
+      if (event.code == "Comma" || event.code == "Enter") {
+        event.preventDefault();
+        var val = event.target.value.trim();
+        if (val.length > 0) {
+          this.tags.push(val);
+          event.target.value = "";
+        }
+      }
+    },
+    removeTag(index) {
+      this.tags.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+.tag-input {
+  width: 100%;
+  border: 1px solid #eee;
+  font-size: 0.9em;
+  height: 50px;
+}
+.tag-input_tag {
+  height: 30px;
+  float: left;
+  margin-right: 10px;
+  background-color: #eee;
+  margin-top: 10px;
+  line-height: 30px;
+  padding: 05px;
+  border-radius: 5px;
+}
+
+.tag-input_tag > span {
+  cursor: pointer;
+  opacity: 0.75;
+}
+
+.tag-input_tag > span {
+  cursor: pointer;
+  opacity: 0.75;
+}
+
+.tag-input_text {
+  border: none;
+  outline: none;
+  font-size: 0.9em;
+  line-height: 50px;
+  background: none;
+}
+</style>
---
+++ dvdong/amis_crm/src/components/layout/TheContent.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="content">
+    <div class="table__content">
+      <div class="abc">
+        <table class="table">
+          <thead>
+            <tr>
+              <th class="text-align-right" style="width: 50px">
+                <input class="checkbox" type="checkbox" name="" id="" />
+              </th>
+              <th style="width: 100px" class="text-align-left">Thẻ</th>
+              <th class="text-align-left" style="width: 80px">Xưng hô</th>
+              <th class="text-align-left" style="width: 140px">Họ và tên</th>
+              <th class="text-align-left" style="width: 120px">Chức danh</th>
+              <th class="text-align-left" style="width: 100px">ĐT di động</th>
+              <th class="text-align-left" style="width: 100px">ĐT cơ quan</th>
+              <th class="text-align-left" style="width: 120px">Email cơ quan</th>
+              <th class="text-align-left" style="width: 120px">Email cá nhân</th>
+              <th class="text-align-left" style="width: 100px">Tổ chức</th>
+              <th class="text-align-left" style="width: 100px">Địa chỉ</th>
+              <th class="text-align-left" style="width: 100px">Tỉnh/Thành phố</th>
+              <th class="text-align-left" style="width: 100px">Quận/Huyện</th>
+              <th class="text-align-left" style="width: 100px">Phường/Xã</th>
+              <th class="text-align-left" style="width: 100px">Nguồn gốc</th>
+              <th class="text-align-left" style="width: 100px">Loại hình</th>
+              <th class="text-align-left" style="width: 100px">Lĩnh vực</th>
+              <th class="text-align-left" style="width: 100px">Mô tả</th>
+              </tr>
+          </thead>
+          
+          <tbody>
+            <tr>
+              <td class="text-align-right" style="width: 50px">
+                <input class="checkbox" type="checkbox" name="" id="" />
+              </td>
+              <td style="width: 100px" class="text-align-left"></td>
+              <td class="text-align-left" style="width: 80px">Anh</td>
+              <td class="text-align-left text--blue" style="width: 140px">Đỗ Việt Anh</td>
+              <td class="text-align-left" style="width: 120px">Nhân viên</td>
+              <td class="text-align-left text--blue" style="width: 100px">
+                <div class="text--icon-right">
+                  <div class="icon--tblcall"></div>
+                  <div>0363090912</div>
+                </div>
+              </td>
+              <td class="text-align-left text--blue" style="width: 100px">
+                <div class="text--icon-right">
+                  <div class="icon--tblcall"></div>
+                  <div>098</div>
+                </div>
+              </td>
+              <td class="text-align-left text--blue" style="width: 120px">
+                dovietanh@gmail.com
+              </td>
+              <td class="text-align-left text--blue" style="width: 120px">
+                dovietanh@gmail.com
+              </td>
+              <td class="text-align-left" style="width: 100px">TSB</td>
+              <td class="text-align-left" style="width: 100px">Khu Đầu</td>
+              <td class="text-align-left" style="width: 100px">Thái Bình</td>
+              <td class="text-align-left" style="width: 100px">Hưng Nhân</td>
+              <td class="text-align-left" style="width: 100px">Hưng Hà</td>
+              <td class="text-align-left" style="width: 100px">hưng hà</td>
+              <td class="text-align-left" style="width: 100px">hưng hà</td>
+              <td class="text-align-left" style="width: 100px">hưng hà</td>
+              <td class="text-align-left" style="width: 100px">
+                hưng hà
+              </td>
+              
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="navigate">
+        <div class="navigate__left">
+          Tổng số: <span style="font-weight: 700">893</span>
+        </div>
+        <div class="navigate__right">
+          <select class="navigate__select" name="" id="">
+            <option value="">50 bản ghi trên trang</option>
+            <option value="">50 bản ghi trên trang</option>
+          </select>
+          <div class="icon--pre2"></div>
+          <div class="icon--pre1"></div>
+
+          <span class="navigate__text" style="font-weight: 700">1</span>
+          đến
+          <span class="navigate__text" style="font-weight: 700">50</span>
+          <div class="icon--next1"></div>
+          <div class="icon--next2"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("./../../css/main.css");
+</style>
