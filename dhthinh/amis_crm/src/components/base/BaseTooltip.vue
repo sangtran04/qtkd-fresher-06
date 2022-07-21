a5b5647b39788c98bd51585f76c262064973d72e
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2022-07-21 09:45:53 +0700

    feat: Cập nhật component BaseTooltip

---
+++ dhthinh/amis_crm/src/components/base/BaseTooltip.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="crm-tooltip" data-tooltip="thinh day nha"></div>
+</template>
+
+<script>
+export default {
+  name: "BaseTooltip",
+  props: {
+    tooltipContent: {
+      type: String,
+      default: "tool tip",
+    },
+    isLeft: {
+        type: Boolean,
+        default: "false",
+    },
+    isRight: {
+        type: Boolean,
+        default: "false",
+    },
+    isTop: {
+        type: Boolean,
+        default: "false",
+    },
+    isBottom: {
+        type: Boolean,
+        default: "false",
+    },
+  },
+
+  computed: {
+    
+  }
+};
+</script>
+
+<style>
+.crm-tooltip {
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+div[data-tooltip] {
+  border-bottom: 1px dotted #333;
+  position: relative;
+}
+div[data-tooltip]::before {
+  content: attr(data-tooltip);
+  position: absolute;
+  background-color: #333;
+  font-size: 13px;
+  color: #fff;
+  text-align: center;
+  padding: 3px 5px;
+  border-radius: 4px;
+  bottom: 0;
+  line-height: 1rem;
+  left: -40%;
+  right: -40%;
+  transform: scale(0);
+  opacity: 0;
+  transition: 0.25s ease-in-out;
+}
+div[data-tooltip]::after {
+  position: absolute;
+  content: "";
+  width: 0;
+  height: 0;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 5px solid #333;
+  left: calc(50% - 2.5px);
+  bottom: 100%;
+  transform: scale(0);
+  opacity: 0;
+  transition: 0.25s ease-in-out;
+}
+
+/* Mặc định thì cho ẩn đi khi hover chuột vào mới hiện lên */
+div[data-tooltip]:hover::before {
+  transform: scale(1);
+  bottom: 100%;
+  opacity: 1;
+}
+div[data-tooltip]:hover::after {
+  transform: scale(1);
+  bottom: calc(100% - 5px);
+  opacity: 1;
+}
+</style>
--- dhthinh/amis_crm/src/components/layout/TheFunction.vue
+++ dhthinh/amis_crm/src/components/layout/TheFunction.vue
@@ -8,7 +8,7 @@
             </div>
             <div class="layout__center"></div>
             <div class="function__right">
-                <div class="buttonadd mgr-8">
+                <div class="buttonadd mgr-8" data-tooltip="Đây là button">
                     <div class="button__plus">
                         <div class="icon-plus"></div>
                     </div>
@@ -32,10 +32,11 @@
 </template>
 
 <script>
+import BaseTooltip from "../base/BaseTooltip.vue";
 export default {
     name: 'TheFunction',
     component: {
-
+        BaseTooltip
     }
 }
 </script>
@@ -72,6 +73,58 @@ export default {
 
 .function__right {
     display: flex;
+}
+
+
+.crm-tooltip {
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+div[data-tooltip] {
+  position: relative;
+}
+div[data-tooltip]::before {
+  content: attr(data-tooltip);
+  position: absolute;
+  background-color: #333;
+  font-size: 13px;
+  color: #fff;
+  text-align: center;
+  padding: 3px 5px;
+  border-radius: 4px;
+  bottom: 0;
+  line-height: 1rem;
+  transform: scale(0);
+  opacity: 0;
+  transition: 0.25s ease-in-out;
+}
+div[data-tooltip]::after {
+  position: absolute;
+  content: "";
+  width: 0;
+  height: 0;
+  border-left: 5px solid transparent;
+  border-right: 5px solid transparent;
+  border-top: 5px solid #333;
+  left: calc(50% - 2.5px);
+  bottom: 100%;
+  transform: scale(0);
+  opacity: 0;
+  transition: 0.25s ease-in-out;
+}
+
+/* Mặc định thì cho ẩn đi khi hover chuột vào mới hiện lên */
+div[data-tooltip]:hover::before {
+  transform: scale(1);
+  bottom: 100%;
+  opacity: 1;
+}
+div[data-tooltip]:hover::after {
+  transform: scale(1);
+  bottom: calc(100% - 5px);
+  opacity: 1;
 }
 
 </style>
