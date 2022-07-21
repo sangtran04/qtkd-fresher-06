3eefc0a16919306093c525a5707bcb14985856ff
Thinh Dang <Thinh Dang@DESKTOP-THINH>
2022-07-21 18:01:19 +0700

    feat: Cập nhật thêm tính năng cho BaseTags

--- dhthinh/amis_crm/src/components/base/BaseTags.vue
+++ dhthinh/amis_crm/src/components/base/BaseTags.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="menu__item" :class="{menu__active: isChecked}">
+    <div class="crm-tags">
+        <div class="crm-tag" :class="{tag__active: isChecked}">
         <slot></slot>
-        <div class="context">
+        <div class="tag__context">
             {{tagContent}} 
         </div>
-    </div>                 
+    </div> 
+    </div>                
 </template>
 
 <script>
@@ -32,27 +34,34 @@ export default {
 </script>
 
 <style>
-
-    .menu__item {
+    .crm-tags {
         cursor: pointer;
+        padding: 12px 16px 0px 16px;
+    }
+
+    .crm-tags:hover {
+        background-color: #f0f2f4;
+        border-radius: 4px;
+    }
+
+    .crm-tag {
         display: flex;
-        margin-left: 16px;
-        height: 42px;
         align-items: center;
+        padding-bottom: 12px;
         justify-content: center;
     }
 
-    .context {
+    .tag__context {
         color: #1F2229;
-        margin-left: 4px;
+        margin-left: 8px;
         font-size: 13px;
     }
 
-    .menu__active {
+    .tag__active {
         border-bottom: solid 2px #4262F0;
     }
 
-    .menu__active > .context {
+    .tag__active > .tag__context {
         color: #4262F0;
     }
 </style>
